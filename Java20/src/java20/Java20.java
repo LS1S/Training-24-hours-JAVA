0d3276952ec5883a3bbb47991a2ddb365bd1bba1
LS1S <117425894+LS1S@users.noreply.github.com>
2023-08-16 14:31:15 +0400

    Update Java20.java

--- Java20/src/java20/Java20.java
+++ Java20/src/java20/Java20.java
@@ -14,7 +14,12 @@ public class Java20 {
         String actore3 = "Джон Херд";
         String role4 = "Нова";
         String acter4 = "Кэсси Сербо";
-        System.out.println(title + "");
+        System.out.println(title + " (" + year + ")\n" +
+                          "Режисёр" + "\t" + directore
+                          role1 + "\t" + acter1 + "\n" +
+                          role2 + "\t" + acter2 + "\n" +
+                          role3 + "\t" + acter3 + "\n" +
+                          role4 + "\t" + acter4);
         
     }
     
